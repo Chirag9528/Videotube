f3ff97ddb892395a744f2ab5de6c3565de43dc75
Chirag9528 <chiragvarshney9528@gmail.com>
2025-01-09 14:41:27 +0530

    Project SetUp

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <>
+    <h1>VideoTube: A Youtube-like video streaming platform</h1>
+    </>
+  )
+}
+
+export default App
